1937564b6c3713b164b23321e702c552d954e2b6
fauzzzzz <fauzanrahmat26th@gmail.com>
2026-07-18 16:48:05 +0200

    Add files via upload

--- part 1/backend.tf
+++ part 1/backend.tf
@@ -4,7 +4,7 @@ terraform {
     key            = "zspace.tfstate" 
     region         = "eu-central-1"                          
     #dynamodb_table = "zspace-dynamo-lock"             #this modified cause deprecated output notification 
-    encrypt        = true
     use_lockfile = true                                #use this instead for state locking directly in s3
+    encrypt        = true
   }
 }
